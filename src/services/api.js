4602f7413046b6106c14fdd8b52355f5193a1395
Keloy <keloy@email.com>
2026-02-17 18:07:19 -0300

    config api url prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000',
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 60000
 });
 
